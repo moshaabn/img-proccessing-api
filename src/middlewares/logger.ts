3a6152b984a019d50125679843ab111c8a93a1de
mohamed shaaban <algaid3@gmail.com>
2022-12-25 01:01:16 +0200

    fix errors

--- src/middlewares/logger.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-const logger = (req: Request, res: Response, next: NextFunction): void => {
-    console.log(req.ip);
-    next();
-};
-
-export default logger;
